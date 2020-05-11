bcd755c68dce9fda5de3628d5da6588d78a115ae
Jacob Heftmann <jheftmann@mac.com>
2020-05-11 16:42:14 -0700

    filter updates

--- assets/application.js
+++ assets/application.js
@@ -328,18 +328,6 @@ $('.page-filter').on('click', function(e) {
   $(find).find('span#title-categories').text('Everything');
 });
 
-$(".page-filter").mouseenter(function(){
-  if ($('span#title-categories').text().indexOf('Everything') > -1){
-    $(this).addClass('disable-hover');
-  }
-});
-
-$(".page-filter").mouseleave(function(){
-  if ($('span#title-categories').text().indexOf('Everything') > -1){
-    $(this).removeClass('disable-hover');
-  }
-});
-
 $('.sub-menu').each(function(i, buttonGroup) {
   var $buttonGroup = $(buttonGroup);
   $buttonGroup.on('click', '.ui-pill', function(e) {
