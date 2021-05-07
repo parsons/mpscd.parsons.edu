1463888f7483814aab16351f774cae9133c14871
Jacob Heftmann <jheftmann@mac.com>
2021-05-07 15:57:26 -0500

    add confetti!

--- assets/application.js
+++ assets/application.js
@@ -457,6 +457,38 @@ var x = setInterval(function() {
     $('.countdown-timer').addClass('countdown-hidden');
     $('.countdown-hero').removeClass('countdown-hidden').addClass('countdown-visible');
     console.log('timer done');
+
+    // confetti({
+    //   particleCount: 150
+    // });
+
+    // TRIGGER CONFETTI!
+    // do this for 30 seconds
+    var duration = 12 * 1000;
+    var end = Date.now() + duration;
+
+    (function frame() {
+      // launch a few confetti from the left edge
+      confetti({
+        particleCount: 7,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 }
+      });
+      // and launch a few from the right edge
+      confetti({
+        particleCount: 7,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 }
+      });
+
+      // keep going until we are out of time
+      if (Date.now() < end) {
+        requestAnimationFrame(frame);
+      }
+    }());
+
   }
 }, 1000);
 
