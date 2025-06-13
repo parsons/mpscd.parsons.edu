4b5ea3ea7deee1f877274f5341812eae31a9ed85
Michael Fehrenbach <mf@michaelfehrenbach.com>
2025-06-13 18:58:11 -0400

    Big ol’ cleanup

--- assets/application.js
+++ assets/application.js
@@ -2,19 +2,6 @@
 title: js
 ---
 
-/* v0
-*
-*= require _lib/v0/v0
-*= require _lib/touch/jquery.ui.touch-punch.min
-*= require _lib/ajaxchimp/jquery.ajaxchimp
-*= require _lib/ajaxchimp/jquery.ajaxchimp.langs
-*= require _lib/jquery.finger/jquery.finger
-*= require _lib/hypher/hypher
-*= require _lib/hypher/en-us
-*= require _lib/marquee/marquee
-*
-*/
-
 !function($) {
 }(window.jQuery);
 
