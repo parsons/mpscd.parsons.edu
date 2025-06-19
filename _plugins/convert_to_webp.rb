5b5c908f4b14e3e4dcbebaf88e8ad94c6eab5614
Michael Fehrenbach <mf@michaelfehrenbach.com>
2025-06-19 17:03:13 -0400

    Loads of telemetry

--- _plugins/convert_to_webp.rb
+++ _plugins/convert_to_webp.rb
@@ -49,6 +49,22 @@ module Jekyll
 				# Otherwise ImageMagick gives us a new asset.
 				output = `magick #{Shellwords.escape(filename)} -resize 2000x2000> -quality 90 -define webp:lossless=false #{Shellwords.escape(webp_file)} 2>&1`
 
+				# Debug: check if input file exists
+				unless File.exist?(filename)
+					Jekyll.logger.error 'Error', "Input file does not exist: #{filename}"
+				end
+				# Debug: check magick command path
+				magick_check = `which magick 2>&1`
+				Jekyll.logger.info 'Magick path', magick_check.strip
+
+				# Debug: print the command string
+				cmd = "magick #{Shellwords.escape(filename)} -resize 2000x2000> -quality 90 -define webp:lossless=false #{Shellwords.escape(webp_file)}"
+				Jekyll.logger.info 'Command', cmd
+
+				# Debug: check WebP support in ImageMagick
+				webp_support = `magick -list format | grep -i webp 2>&1`
+				Jekyll.logger.info 'WebP support', webp_support.strip
+
 				if $?.success? && File.exist?(webp_file)
 					Jekyll.logger.info 'Converted', "#{File.basename(webp_file)}"
 				else
