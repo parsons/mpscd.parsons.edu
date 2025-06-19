5c3231350dceb2bafaa372e1d03a7bcd86dfaff5
Michael Fehrenbach <mf@michaelfehrenbach.com>
2025-06-19 17:39:34 -0400

    Simplify telemetry

--- _plugins/convert_to_webp.rb
+++ _plugins/convert_to_webp.rb
@@ -47,26 +47,12 @@ module Jekyll
 				next
 			else
 				# Otherwise ImageMagick gives us a new asset.
-				cmd = "magick #{Shellwords.escape(filename)} -resize '2000x2000>' -quality 90 -define webp:lossless=false #{Shellwords.escape(webp_file)}"
-				Jekyll.logger.info 'Command', cmd
-				output = `#{cmd} 2>&1`
-
-				# Debug: check if input file exists
-				unless File.exist?(filename)
-					Jekyll.logger.error 'Error', "Input file does not exist: #{filename}"
-				end
-				# Debug: check magick command path
-				magick_check = `which magick 2>&1`
-				Jekyll.logger.info 'Magick path', magick_check.strip
-
-				# Debug: check WebP support in ImageMagick
-				webp_support = `magick -list format | grep -i webp 2>&1`
-				Jekyll.logger.info 'WebP support', webp_support.strip
+				result = `magick #{Shellwords.escape(filename)} -resize '2000x2000>' -quality 90 -define webp:lossless=false #{Shellwords.escape(webp_file)} 2>&1`
 
 				if $?.success? && File.exist?(webp_file)
 					Jekyll.logger.info 'Converted', "#{File.basename(webp_file)}"
 				else
-					Jekyll.logger.error 'Error', "Failed to convert #{File.basename(filename)}. Output: #{output}"
+					Jekyll.logger.error 'Error', "Failed to convert #{File.basename(filename)}!"
 				end
 			end
 		end
